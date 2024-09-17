75a58fb98b52e9d0bc0a873980e0f69f2b9c343f
S Gopi <gopi.s@seaedu.ac.in>
2024-09-17 21:06:34 +0530

    Redefining app routes

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,62 +1,13 @@
-import {
-  createBrowserRouter, 
-  RouterProvider,
-} from "react-router-dom";
-import Header from "./components/Header";
-import Signin from "./pages/signin";
-import ProtectedRoute from "./ProtectedRoute";
-import Notfound from './pages/Notfound';
-import Signup from './pages/signup';
-import Dashboard from "./pages/dashboard";
+// import React from "react";
+import { RouterProvider } from "react-router-dom";
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Signup />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/signin",
-    element: <Signin />,
-  },
-  {
-    path: "/notfound",
-    element: <Notfound />,
-  },
-  {
-    path: "/dashboard",
-    element: (
-      <ProtectedRoute>
-        <Dashboard />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "*",
-    element: <Notfound />,
-  }
-]);
+import router from "./routes"
 
 const App = () => {
-  const isHeaderVisible = true;
   return (
-    
-     
-      <div>
-      {isHeaderVisible && <Header />}
-      {/* <Form /> */}
-
-        {/* <ReactPlayground /> */}
-        <RouterProvider router={router} />
-
+    <div>
+      <RouterProvider router={router} />
     </div>
-
-    
   );
-};
-
+}
 export default App;
---
+++ smarter-tasks/src/pages/members/index.tsx
@@ -0,0 +1,8 @@
+const Members = () => {
+    return (
+      <h2>Members</h2>
+    );
+  };
+  
+  export default Members;
+  
