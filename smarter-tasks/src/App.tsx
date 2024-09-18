57f361e4a63632c934496a852d2fa41143e3f073
S Gopi <gopi.s@seaedu.ac.in>
2024-09-18 12:50:31 +0530

    Creating our first context

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,11 +1,14 @@
-// import React from "react";
 import { RouterProvider } from "react-router-dom";
+import { useContext } from "react";
+import ThemeContext from "./context/theme";
 
 import router from "./routes"
 
 const App = () => {
+  const currentTheme = useContext(ThemeContext)
   return (
     <div>
+      {currentTheme}
       <RouterProvider router={router} />
     </div>
   );
