1e19644f89ff6f44326b714620c625e04dbf59ad
S Gopi <skopeemail@gmail.com>
2024-09-20 22:29:36 +0530

    added level 7 files

--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -7,7 +7,7 @@ import ProtectedRoute from "./ProtectedRoutes"
 import Projects from "../pages/projects";
 import Members from "../pages/members";
 import Logout from "../pages/logout";
-import NotFound from "../pages/NotFound";
+import NotFound from "../pages/Notfound";
 
 
 const router = createBrowserRouter([
